448cc36c8616701c22db55bab192301e8723ef52
Nacer <kichounacer2003@gmail.com>
2026-08-08 13:55:37 +0100

    Corrige le logo (logo-orania.png au lieu de logo-qreeb.png)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo({ size = 40 }: { size?: number }) {
       className="flex items-center justify-center rounded-2xl shrink-0 bg-white"
       style={{ width: size, height: size, boxShadow: "0 2px 10px rgba(16,31,61,0.12)" }}
     >
-      <img src="/logo-qreeb.png" alt="QREEB" style={{ width: size * 0.68, height: size * 0.68 }} className="object-contain" />
+      <img src="/logo-orania.png" alt="Orania" style={{ width: size * 0.68, height: size * 0.68 }} className="object-contain" />
     </div>
   );
 }
